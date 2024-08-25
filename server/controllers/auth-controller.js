892ed117acfb199d7826f52351834bd373c6c15f
Bappy-Hossain <bh78764@gmail.com>
2024-08-25 17:27:51 +0900

    login system added

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,35 +1,61 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
 
 //Homepage
-const home = async(req,res) => {
-    try {
-        res.status(200).send("Welcome to this homepage...")
-    } catch (error) {
-        console.log(error)
-    }
-}
+const home = async (req, res) => {
+  try {
+    res.status(200).send("Welcome to this homepage...");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //Register page
-const register = async(req,res) => {
-    try {
-        const {username,email,phone,password} = req.body;
+const register = async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
 
-        const userExists = await User.findOne({email: email});
+    const userExists = await User.findOne({ email: email });
 
-        if(userExists){
-            return res.status(400).json({msg: "Email already exists!"})
-        }
-
-        const userCreated = await User.create({username,email,phone,password});
-
-        res.status(201).json({
-            msg: "Registration successfull!",
-            token: await userCreated.generateToken(),
-            userId: userCreated._id.toString()});
-
-    } catch (error) {
-        res.status(500).json("Internal server error!")
+    if (userExists) {
+      return res.status(400).json({ msg: "Email already exists!" });
     }
-}
 
-module.exports = {home,register};
+    const userCreated = await User.create({ username, email, phone, password });
+
+    res.status(201).json({
+      msg: "Registration successfull!",
+      token: await userCreated.generateToken(),
+      userId: userCreated._id.toString(),
+    });
+  } catch (error) {
+    res.status(500).json("Internal server error!");
+  }
+};
+
+//Login page
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userExist = await User.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).json("Invalid Credentials!");
+    }
+
+    const user = await bcrypt.compare(password, userExist.password);
+    if (user) {
+      res.status(200).json({
+        msg: "Login successfull!",
+        token: await userExist.generateToken(),
+        userId: userExist._id.toString(),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password!" });
+    }
+  } catch (error) {
+    res.status(500).json("Internal server error!");
+  }
+};
+
+module.exports = { home, register, login };
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authControllers = require("../controllers/auth-controller");
 
-router.get("/",authControllers.home);
-router.post("/register",authControllers.register);
+router.get("/", authControllers.home);
+router.post("/register", authControllers.register);
+router.post("/login", authControllers.login);
 
 module.exports = router;
