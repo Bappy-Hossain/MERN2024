f1654b97630e377b30a08d0d3254113799a1eccc
Bappy-Hossain <bh78764@gmail.com>
2024-09-02 23:27:49 +0900

    home page create frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,90 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <h1>This is homepage...</h1>
+      <main>
+        <section className="section-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>We Are The World Best IT Company</p>
+              <h1>Welcome To Bappy Ahmed</h1>
+              <p>
+                Are you ready to take your business to the next level with
+                cutting-edge IT solutions? Look no further! Ath Bappy Ahmed, we
+                specialize in providing innovative IT services and solutions
+                tailored to meet your unique needs.
+              </p>
+              <div className="btn btn-group">
+                <a href="/contact">
+                  <button className="btn">Connect Now</button>
+                </a>
+                <a href="/about">
+                  <button className="btn secondary-btn">Learn More</button>
+                </a>
+              </div>
+            </div>
+            <div className="hero-image">
+              <img
+                src="/images/home.png"
+                alt="hero_image"
+                width="400"
+                height="500"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <section className="section-analytics">
+        <div className="container grid grid-four-cols">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>Registered Companies</p>
+          </div>
+          <div className="div1">
+            <h2>10,000+</h2>
+            <p>Happy Clients</p>
+          </div>
+          <div className="div1">
+            <h2>500+</h2>
+            <p>Well Known Developers</p>
+          </div>
+          <div className="div1">
+            <h2>24/7</h2>
+            <p>Service</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="section-hero">
+        <div className="container grid grid-two-cols">
+          <div className="hero-image">
+            <img
+              src="/images/design.png"
+              alt="hero_image"
+              width="400"
+              height="500"
+            />
+          </div>
+          <div className="hero-content">
+            <p>We are here to help you</p>
+            <h1>Get Started Today</h1>
+            <p>
+              Ready to take the first step towards a more efficient and secure
+              IT infrastructure? Contact us today for a free consultation and
+              let's discuss how Bappy Ahmed can help your business thrive in the
+              digital age.
+            </p>
+            <div className="btn btn-group">
+              <a href="/contact">
+                <button className="btn">Connect Now</button>
+              </a>
+              <a href="/about">
+                <button className="btn secondary-btn">Learn More</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
