f71bb2ebd0fa45622703b878c9027ad3b97de53c
Bappy-Hossain <bh78764@gmail.com>
2024-09-26 12:52:21 +0900

    some correction

--- server/src/validators/index.js
+++ server/src/validators/index.js
@@ -1,15 +1,15 @@
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 
-const runValidation = async (req,res,next) => {
-    try {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(403).json({message: errors.array()[0].msg});
-        }
-        return next();
-    }catch (error) {
-        return res.status(400).json("Validation failed!")
+const runValidation = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(403).json({ message: errors.array()[0].msg });
     }
-}
+    return next();
+  } catch (error) {
+    return res.status(400).json("Validation failed");
+  }
+};
 
 module.exports = runValidation;
